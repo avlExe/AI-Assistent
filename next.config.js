09e9467b1ceda5f8942353e0f2f582b65337adaf
Pavel <avel.strelnikov@outlook.com>
2025-09-07 15:53:24 +0700

    Configure Next.js for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
+  // Для Netlify
+  trailingSlash: true,
+  distDir: 'out',
+  output: 'export'
 }
 
 module.exports = nextConfig
